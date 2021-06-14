49c32edeb7ca46e7e69b18de101cf587b6036d7b
Xin Peng <xin_peng@outlook.com>
2021-06-14 18:21:57 -0500

    Bump version: 0.0.4 → 0.1.0

--- pydiggs/__init__.py
+++ pydiggs/__init__.py
@@ -2,6 +2,6 @@
 
 __author__ = """Xin Peng"""
 __email__ = 'xin_peng@outlook.com'
-__version__ = '0.0.4'
+__version__ = '0.1.0'
 
 from .pydiggs import *
--- setup.py
+++ setup.py
@@ -44,6 +44,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/xinp-hub/pydiggs',
-    version='0.0.4',
+    version='0.1.0',
     zip_safe=False,
 )
