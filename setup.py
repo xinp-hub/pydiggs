433c6e38942ae78e9662eaba00a680267c8fd399
Xin Peng <xin_peng@outlook.com>
2021-06-21 19:50:30 -0500

    uupdated setup.py to incorperate all dependencies

--- setup.py
+++ setup.py
@@ -10,7 +10,8 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [ ]
+with open('requirements.txt') as requirements_file:
+    requirements = requirements_file.read().splitlines()
 
 test_requirements = ['pytest>=3', ]
 
